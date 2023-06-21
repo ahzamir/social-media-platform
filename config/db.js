b74290544ceb15ff66e525e7495f461104571e74
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2023-06-21 17:07:31 +0430

    added the configuration for conecting to MonfoDB

--- config/db.js
+++ config/db.js
@@ -5,7 +5,10 @@ const MONGODB_URI = process.env.MONGODB_URI;
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(MONGODB_URI);
+        await mongoose.connect(MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log('MongoDB connected!');
     } catch (err) {
         console.log(err.message);
