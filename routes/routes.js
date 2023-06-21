e62820f2f585fcbf5eea4e1a9bd07b407c583fea
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2023-06-21 17:37:22 +0430

    created the signup route (endpoint)

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const passport = require('passport');
+
 const router = express.Router();
-const User = require('../models/user');
 
 router.get('/login', (req, res) => {
     res.send('Login page');
